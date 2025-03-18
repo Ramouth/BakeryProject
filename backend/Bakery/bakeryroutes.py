99274d1c082959751a6df0add5cab7dbbf9aedbe
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2025-03-18 12:31:16 +0100

    comment for bp

--- backend/Bakery/bakeryroutes.py
+++ backend/Bakery/bakeryroutes.py
@@ -3,6 +3,9 @@ from . import bakery_bp
 from config import db
 from models import Bakery
 
+# Routes for CRUD operations for Bakery
+# Notice that we are now using @bakery_bp instead of @app and the route is now "/" because of url prefix in the blueprint 
+
 @bakery_bp.route("/", methods=["GET"])
 def get_bakeries():
     bakeries = Bakery.query.all()
