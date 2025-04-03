75b0a31a1feee272bdbdd3433b127e633673d69f
MortenBeck <s224572@dtu.dk>
2025-04-03 02:13:55 +0200

    slight update to navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -21,13 +21,31 @@ const NavBar = () => {
         </div>
         
         <div className="header-actions">
-          <Link to="/login" className="login-button">
-            Log in
-          </Link>
-          
-          <Link to="/bakery-selection" className="sign-up-button">
-            Sign up
-          </Link>
+          {currentUser ? (
+            <>
+              <Link to="/bakery-selection" className="review-button">
+                <span className="plus-icon">+</span>
+                <span className="separator"></span>
+                <span>Review</span>
+              </Link>
+              <Link to="/profile" className="profile-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </svg>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/login" className="login-button">
+                Log in
+              </Link>
+              
+              <Link to="/bakery-selection" className="sign-up-button">
+                Sign up
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </header>
